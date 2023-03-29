60749fe8d4bf5b1df5b3691115b33b3ef3660aaf
amimer22 <devealboy@gmail.com>
2023-03-29 02:14:23 +0200

    main

---
+++ src/main/scala/riscv/Main.scala
@@ -0,0 +1,25 @@
+package riscv 
+
+class Main extends Module {
+    val io = IO(new Bundle {
+
+    })
+    //module objects
+    val IDecode = Module(new IDecode())
+    val Registers = Module(new Registers())
+    val myIOP = Module(new IOP())
+    val ALU = Module(new ALU())
+
+    Registers.io.rs1addr := IDecode.io.rs1
+    Registers.io.rs2addr := IDecode.io.rs2
+    Registers.io.rdaddr := IDecode.io.rd
+    Registers.io.WE := IOP.io.WE
+    Registers.io.result := ALU.io.result
+
+    IOP.io.opcode := IDecode.io.opcode
+    IOP.io.funct3 := IDecode.io.funct3
+    IOP.io.funct7 := IDecode.io.funct7
+
+    ALU.io.operation = IOP.io.operation
+
+}
